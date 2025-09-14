9461f6790673e143a6a155bdf009fd71737e1a03
Mitesh T <mtrivedi@zacks.com>
2025-09-14 16:39:34 +0530

    cors issue

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ async def health_check():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="*",
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
